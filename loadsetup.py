2a561c55af982ebbbd33412bc15f1ea9437629be
michaeljfriedman <mfred240@gmail.com>
2016-08-12 14:44:50 -0400

    Allow user to name the workspace when loading setup
    - After selecting the setup, the user is prompted to type a
    name for the workspace. They can leave it blank to use the
    setup name.

--- loadsetup.py
+++ loadsetup.py
@@ -16,6 +16,15 @@ def run(cmd):
 	call(cmd)
 	I3.waste_some_time()
 
+# Stips the newline character off the end of a line
+def with_newline_stripped(text):
+	NEWLINE = "\n"
+	N = len(text)
+	if NEWLINE == text[N-1]:
+		return text[0:N-1]
+	else:
+		return text
+
 # Renames the workspace of the provided number w to the given name
 def rename_workspace(w, new_name):
 	workspace_declaration = I3.workspace_declaration(w)
@@ -49,10 +58,10 @@ print "--------------------------------"
 setups = s.create_setups()
 
 # Prompt user to select a setup
-prompt_message = "Select a setup from the following options:\n"
+prompt_select_setup_msg = "Select a setup from the following options:\n"
 for i in range(0, len(setups.names())):
-	prompt_message += str(i+1) + ": " + setups.names()[i] + "\n"
-print prompt_message
+	prompt_select_setup_msg += str(i+1) + ": " + setups.names()[i] + "\n"
+print prompt_select_setup_msg
 selection = 0
 num_attempts = 1
 MAX_ATTEMPTS = 5
@@ -68,9 +77,14 @@ while selection not in range(1, len(setups.names()) + 1):
 selection_index = selection - 1
 
 # Rename the workspace and restart i3 to apply that name
-rename_workspace(workspace_num, setups.names()[selection_index])
+prompt_rename_workspace_msg = "Enter a name for the workspace (or leave it blank to use the setup name)"
+print prompt_rename_workspace_msg
+workspace_name = with_newline_stripped(stdin.readline())
+if workspace_name == "":
+	workspace_name = setups.names()[selection_index]
+rename_workspace(workspace_num, workspace_name)
 I3.msg("reload")
-I3.msg("workspace " + str(workspace_num) + ": " + setups.names()[selection_index])
+I3.msg("workspace " + str(workspace_num) + ": " + workspace_name)
 
 # Run the commands for the selected setup
 for cmd in setups.cmds(selection_index):
