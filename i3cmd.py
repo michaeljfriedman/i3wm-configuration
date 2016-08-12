9cb03a33734e6de30e404432412af922e42cfda4
michaeljfriedman <mfred240@gmail.com>
2016-08-12 01:41:32 -0400

    Added renaming of workspaces when loading a setup
    - When a setup is loaded on a workspace, the setup now changes
    the name of the workspace to match the setup loaded on it
    - A backup of the config file with default workspace names is
    saved to config.on-startup
    - config.on-startup is never restored, however (user must
    manually restore it to get original workspace names back)

--- i3cmd.py
+++ i3cmd.py
@@ -5,13 +5,21 @@ Created: 08/10/2016
 Description:
 This module allows a client to pass in any i3 command and execute it.
 '''
-from os import system as run
+from subprocess32 import call as run
 from sys import argv
+
+def waste_some_time():
+	for i in range(0, 10000000):
+		a = 1
 
 # Executes an i3 command
 def i3cmd(cmd):
 	i3_command_runner = "i3-msg"
-	run(i3_command_runner + " " + cmd)
+	parts = [i3_command_runner]
+	for part in cmd.split(" "):
+		parts.append(part)
+	run(parts)
+	waste_some_time()
 
 
 # Test client runs the i3 command given as argument
--- loadsetup.py
+++ loadsetup.py
@@ -8,9 +8,20 @@ one, and then applies that setup to the workspace number passed as argument.
 '''
 from sys import argv, stdin
 from shutil import copy2			# copy2 preserves all file metadata as well
-from i3cmd import i3cmd
-from os import system as run
+from subprocess32 import call
+from i3cmd import i3cmd, waste_some_time
+import os.path
 import setups as s
+
+I3_DIRECTORY 				= "/home/michael/.config/i3/"
+CONFIG_FILENAME 		= "config"
+CONFIG_FILE 				= I3_DIRECTORY + CONFIG_FILENAME
+CONFIG_FILE_BACKUP 	= CONFIG_FILE + ".on-startup"
+
+# Runs terminal command and then wastes time
+def run(cmd):
+	call(cmd)
+	waste_some_time()
 
 # Returns the index of the string s in the array a
 def index_of(item, a):
@@ -30,29 +41,42 @@ def with_newline_stripped(s):
 	return s
 
 # Copies the initial config file to config.on-startup so it can be restored when
-# the user logs out. Returns a reference to the copy of the file named "config"
-# with rw permissions
+# the user logs out
 def copy_config():
-	copy2("config", "config.on-startup")
-	f = open("config", "rw")
-	return f
+	copy2(CONFIG_FILE, CONFIG_FILE_BACKUP)
 
-# Renames the workspace of the provided number w to the given name in the given
-# config file. Assumes the convention that workspace names are defined in
-# variables of the form $WORKSPACE#
-def rename_workspace(w, new_name, config):
-	workspace_declaration_phrase = "set $WORKSPACE" + str(w)
-	for line in config:
-		if workspace_declaration_phrase in line:
-			line = workspace_declaration_phrase + str(w) + ": " + new_name
-			# TODO: write this line to the config in place of the old line
+# Renames the workspace of the provided number w to the given name. Assumes the
+# convention that workspace names are defined in variables of the form:
+# $WORKSPACE#
+# Returns the full name of the workspace (as named in the i3 config file)
+def rename_workspace(w, new_name):
+	workspace_declaration = "set $WORKSPACE" + str(w)
+	replacement 					= "%s \"%d: %s\"" % (workspace_declaration, w, new_name)
+	config 								= open(CONFIG_FILE, "r")
+	lines 								= config.readlines()
+	found 								= False
+	for i in range(0, len(lines)):
+		if workspace_declaration in lines[i]:
+			del lines[i]
+			lines.insert(i, replacement + "\n")
+			found = True
+			break
+	config.close()
+	if found:
+		config = open(CONFIG_FILE, "w")
+		for line in lines:
+			config.write(line)
+		config.close()
+	else:
+		raise Exception("Query not found in file")
 
 #-------------------
 # Main script
 #-------------------
-# Switch to the specified workspace
 workspace_num = int(argv[1])
-i3cmd("workspace " + str(workspace_num))
+print "--------------------------------"
+print "  Load a setup on workspace %d  " % (workspace_num)
+print "--------------------------------"
 
 # Load in user-defined setups
 setups = s.create_setups()
@@ -73,9 +97,13 @@ while with_newline_stripped(selection) not in setups.names():
 selection_index = index_of(selection, setups.names())
 
 # Rename the workspace and restart i3 to apply that name
-# rename_workspace(workspace_num, setups.names()[selection_index], copy_config())
-# i3cmd("restart")
+if not os.path.isfile(CONFIG_FILE_BACKUP):
+	copy_config()
+rename_workspace(workspace_num, setups.names()[selection_index])
+i3cmd("restart")
+i3cmd("workspace " + str(workspace_num) + ": " + setups.names()[selection_index])
 
 # Run the commands for the selected setup
 for cmd in setups.cmds(selection_index):
-	run(cmd)
+	parts = cmd.split(" ")
+	run(parts)
